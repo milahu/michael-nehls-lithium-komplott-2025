0d0291150f158e88be3aceea8bcaa81a46f7c200
Milan Hauth <milahu@milahu.duckdns.org>
2026-07-06 20:55:19 +0200

    up 100-hocr2epub.sh

--- 100-hocr2epub.sh
+++ 100-hocr2epub.sh
@@ -45,27 +45,64 @@ args+=(
   --scale "$scale"
   --image-format avif
   --text-format html
-  --doc-title "$doc_title"
   --doc-modified "$doc_modified"
-  --doc-isbn 0000000000000
-)
-todo_args+=(
-  --doc-title ""
-  --doc-subtitle ""
-  --doc-description ""
-  --doc-subject ""
-  --doc-date 2025
-  --doc-edition 1
-  --doc-extent "123 pages"
-  --doc-author ""
-  --doc-introducer ""
-  --doc-contributor ""
-  --doc-translator ""
-  --doc-publisher ""
+  --color-image-pages 465,466,469
+  --doc-title "Das Lithium-Komplott"
+  --doc-subtitle "Plädoyer für ein essentielles Spurenelement - Der verbotene Schlüssel zur mentalen Gesundheit"
+  --doc-description "Eine medizinische und gesellschaftliche Revolution.
+
+Lithium ist ein Spurenelement,
+das seit Urzeiten für die Entwicklung allen Lebens unentbehrlich ist.
+Der Mensch bildet da keine Ausnahme.
+
+Aber leider herrscht ein weit verbreiteter Mangel,
+der unsere Lebensqualität mindert.
+Er führt zu einer Zunahme von Depressionen und Alzheimer-Demenz,
+zu vermehrten Einweisungen in psychiatrische Kliniken,
+zu mehr gewalttätigem Verhalten,
+zu höheren Suizidraten und letztlich sogar zu einer verkürzten Lebenserwartung!
+
+Die fatalen Folgen des Mangels werden uns jedoch als unabänderliche Normalität verkauft,
+und gleichzeitig weigern sich die zuständigen Behörden,
+die essentielle Bedeutung dieses Spurenelements anzuerkennen.
+Perfiderweise sind lithiumhaltige Nahrungsergänzungsmittel in Europa – wie auch in den meisten anderen Ländern der Welt – sogar verboten.
+
+Allein die Tatsache,
+dass viele mangelbedingte Verhaltensstörungen in der kindlichen Entwicklung
+meist erfolglos mit nebenwirkungsreichen, aber höchst lukrativen Medikamenten behandelt werden,
+lässt monetäre Interessen hinter diesem Missstand vermuten.
+
+Geht es aber wirklich nur um Profit
+oder haben wir es gar mit einem pervertierten Machtanspruch über den Geist des Menschen zu tun?
+Wem nützt eine Gesellschaft,
+die körperlich und psychisch immer kränker und weniger belastbar wird
+und in chronischer Zukunftsangst lebt?
+
+Das Lithium-Komplott liefert dringend benötigte Antworten
+und ist gleichzeitig ein medizinisches Plädoyer
+für die längst überfällige Anerkennung der essentiellen Bedeutung von Lithium
+als Schlüssel zu einer gesünderen,
+psychisch stabileren und friedensfähigeren Gesellschaft.
+
+Schließlich bietet es einen hochwirksamen,
+natürlichen Schutz gegen chronische Entzündungen,
+hirntoxischen Stress und Angststörungen – Leiden mit neuerdings pandemischem Charakter.
+
+So könnte essentielles Lithium zum Symbol für den Aufbruch der Menschheit in eine neue Ära der Medizin werden,
+die den natürlichen Bedürfnissen des Menschen gerecht wird."
+  --doc-subject "nutrition, health, medicine"
+  --doc-date 2025-04-30
+  --doc-edition 2
+  --doc-extent "464 pages"
+  --doc-author "Michael Nehls"
+  #--doc-introducer ""
+  #--doc-contributor ""
+  #--doc-translator ""
+  --doc-publisher "Mental Enterprises"
   --doc-language de
-  --doc-isbn 0000000000000
-  --doc-cover-image 070-deskew/999.jpg
-  --canonical-url-base https://milahu.github.io/todo/
+  --doc-isbn 9783981404890
+  --doc-cover-image 070-deskew/465.jpg
+  --canonical-url-base https://milahu.github.io/michael-nehls-lithium-komplott-2025/
 )
 
  printf '>'
