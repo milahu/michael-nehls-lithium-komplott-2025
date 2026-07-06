a3b16c7a5b1e118b92103e2de4ed51d1d23f63c7
Milan Hauth <milahu@milahu.duckdns.org>
2026-07-06 20:54:41 +0200

    up 0684-fill-white-pages-config.py

--- 0684-fill-white-pages-config.py
+++ 0684-fill-white-pages-config.py
@@ -2,7 +2,7 @@
 
 # Threshold to consider a page "white" (mean lightness close to 100)
 # WHITE_LIGHTNESS_THRESHOLD = 99.99
-WHITE_LIGHTNESS_THRESHOLD = 99.9
+WHITE_LIGHTNESS_THRESHOLD = 99.95
 
 image_dir = "065-remove-page-borders"
 
